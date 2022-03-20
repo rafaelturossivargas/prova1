45144cb615987fe107f8afcd1fbc78cbdd5e578d
rafaelturossivargas <102000580+rafaelturossivargas@users.noreply.github.com>
2022-03-20 20:46:21 -0300

    Create scanner.java

---
+++ scanner.java
@@ -0,0 +1,12 @@
+pacote  br.ulbra.classes ;
+
+import  java.io.InputStream ;
+
+ Scanner de classe  pública 
+{
+
+    Scanner ( InputStream  em ) {
+        throw  new  UnsupportedOperationException ( " Ainda não suportado. " ); // Para alterar o corpo dos métodos gerados, escolha Tools | Modelos.
+    }
+    
+}
